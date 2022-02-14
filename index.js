55af723c6a827c27072c7705d310efbe9c95aff6
Rafael Costa Palma <rafael.palma@opah.com.br>
2022-02-14 20:17:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const questions = [
+    "O que aprendi hoje?",
+    "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas eu ajudei hoje?"
+]
+
+const ask = ( index = 0 ) => {
+    process.stdout.write(questions[index] + " -> ")
+}
+
+ask();
+
+const answers = [];
+
+process.stdin.on("data", data => {
+    answers.push(data.toString().trim())
+    if (answers.length < questions.length) {
+        ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit()
+    }
+})
+
+process.on('exit', () => {
+    console.log(`
+        Bacana Mayk!
+
+        O que você aprendeu hoje foi:
+        ${answers[0]}
+
+        O que te aborreceu e você poderia melhorar foi:
+        ${answers[1]}
+
+        O que te deixou feliz hoje:
+        ${answers[2]}
+
+        Você ajudou ${answers[3]} pessoas hoje!
+
+        Volte amanha para novas reflexões!
+
+    `)
+})
